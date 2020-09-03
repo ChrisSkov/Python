61a0e88208954ee6d454ca2c281c637fbfc6c429
Chris Skov <stofferdyr@gmail.com>
2020-09-03 14:46:07 +0200

    start lesson 17

---
+++ Python-Udemy/hello-world.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 12:34:47 2020
+
+@author: stoff
+"""
+""" single slash is divide to float"""
+print(7/3)
+""" double slash is divide to int"""
+print(-7//3)
+"""2**8 = 2^8"""
+print(2**8)
+"""" back slash \n = new line \t = tab"""
+"""" use backslash '\' to escape characters in strings"""
+"""" use end = '' to define an end character for a string"""
+
+print("One,'two', \"three\", four, \n \\five\\ once,'I' caught a\n fish '//alive\\\\'")
---
+++ Python-Udemy/variables.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  3 13:05:04 2020
+
+@author: stoff
+"""
+# x = 5
+
+# y = 12
+
+user_input = int(input("How much you bench? \n >>> "))
+
+far = float(input("temp in Fahrenheit: "))
+cel = (far - 32) * 5/9
+print(far, "F in C is: ", cel)
