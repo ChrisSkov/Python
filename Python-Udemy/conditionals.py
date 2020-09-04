152e1a6c12454737ae7f432b8ca3bc793091c4d5
Chris Skov <stofferdyr@gmail.com>
2020-09-04 17:10:46 +0200

    start lecture 21

---
+++ Python-Udemy/conditionals.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 14:31:28 2020
+
+@author: stoff
+"""
+
+var_1 = int(input("input int pls:  "))
+var_2 = int(input("input int pls:  "))
+var_3 = int(input("input int pls:  "))
+
+if var_1 > var_2 and var_3 > var_2:
+    print("big")
+else:
+    print("smol")
--- Python-Udemy/variables.py
+++ Python-Udemy/variables.py
@@ -8,8 +8,22 @@ Created on Thu Sep  3 13:05:04 2020
 
 # y = 12
 
-user_input = int(input("How much you bench? \n >>> "))
+# user_input = int(input("How much you bench? \n >>> "))
 
-far = float(input("temp in Fahrenheit: "))
-cel = (far - 32) * 5/9
-print(far, "F in C is: ", cel)
+# far = float(input("temp in Fahrenheit: "))
+# cel = (far - 32) * 5/9
+# print(far, "F in C is: ", cel)
+
+
+# num1 = int(input("Enter the first int: "))
+# num2 = int(input("Enter the second int: "))
+
+# print("the product of the two numbers is: ", num1 * num2)
+
+total_slices = 4*4
+
+no_of_pizzas = (total_slices + 5)//6
+
+slices_left = no_of_pizzas*6 - total_slices
+
+print("you need", no_of_pizzas, "pizzas", "there will be", slices_left, "slices left")
