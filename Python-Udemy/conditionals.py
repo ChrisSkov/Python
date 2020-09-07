9c01d52b211bdabeb18b531cd68693740f1ab5aa
Chris Skov <stofferdyr@gmail.com>
2020-09-07 13:35:28 +0200

    start lesson 26

--- Python-Udemy/conditionals.py
+++ Python-Udemy/conditionals.py
@@ -5,11 +5,38 @@ Created on Fri Sep  4 14:31:28 2020
 @author: stoff
 """
 
-var_1 = int(input("input int pls:  "))
-var_2 = int(input("input int pls:  "))
-var_3 = int(input("input int pls:  "))
+# var_1 = int(input("input int pls:  "))
+# var_2 = int(input("input int pls:  "))
+# var_3 = int(input("input int pls:  "))
 
-if var_1 > var_2 and var_3 > var_2:
-    print("big")
-else:
-    print("smol")
+# lesson 25 challenge
+# user input int between 1 and 5 inclusive.
+# print out string value
+
+# user_input = int(input("input int between 1 and 5: "))
+
+# if user_input == 1:
+#     print("one")
+# elif user_input == 2:
+#     print("two")
+# elif user_input == 3:
+#     print("three")
+# elif user_input == 4:
+#     print("four")
+# elif user_input == 5:
+#     print("five")
+# else:
+#     print("fuck off with your high ass numbers")
+
+
+secret_number = 4
+
+guess = input("Guess number:")
+if guess.isdigit():
+    guess = int(guess)
+    if guess == secret_number:
+        print("you won..")
+    elif guess > secret_number:
+        print("too high")
+    elif guess < secret_number:
+        print("too low")
